95d191e23b5e76a0e53238839b4b3f6ad83d0c04
relentlessreed <relentlessreed@gmail.com>
2021-04-29 19:04:50 -0500

    refactored code

---
+++ mockdata.js
@@ -0,0 +1,334 @@
+{
+    "results": [
+        {
+            "vegetarian": true,
+            "vegan": false,
+            "glutenFree": true,
+            "dairyFree": true,
+            "veryHealthy": false,
+            "cheap": false,
+            "veryPopular": false,
+            "sustainable": false,
+            "weightWatcherSmartPoints": 13,
+            "gaps": "no",
+            "lowFodmap": false,
+            "aggregateLikes": 295,
+            "spoonacularScore": 94,
+            "healthScore": 34,
+            "creditsText": "Lisa's Vegetarian Kitchen",
+            "license": "CC BY 2.5 CA",
+            "sourceName": "Food and Spice",
+            "pricePerServing": 319.19,
+            "id": 716217,
+            "title": "Vietnamese Pancakes with Vegetables, Herbs and a Fragrant Dipping Sauce (Bánh Xèo)",
+            "readyInMinutes": 45,
+            "servings": 6,
+            "sourceUrl": "http://foodandspice.blogspot.com/2012/05/vietnamese-pancakes-with-vegetables.html",
+            "image": "https://spoonacular.com/recipeImages/716217-312x231.jpg",
+            "imageType": "jpg",
+            "summary": "The recipe Vietnamese Pancakes with Vegetables, Herbs and a Fragrant Dipping Sauce (Bánh Xèo) is ready <b>in approximately 45 minutes</b> and is definitely an awesome <b>gluten free, dairy free, and vegetarian</b> option for lovers of Vietnamese food. For <b>$3.19 per serving</b>, this recipe <b>covers 27%</b> of your daily requirements of vitamins and minerals. This recipe makes 6 servings with <b>377 calories</b>, <b>8g of protein</b>, and <b>21g of fat</b> each. It will be a hit at your <b>The Super Bowl</b> event. 295 people were impressed by this recipe. Head to the store and pick up sesame oil, sea salt, egg, and a few other things to make it today. It works well as a side dish. All things considered, we decided this recipe <b>deserves a spoonacular score of 94%</b>. This score is outstanding. Try <a href=\"https://spoonacular.com/recipes/bnh-xo-vietnamese-happy-pancakes-891106\">Bánh xèo (Vietnamese Happy Pancakes)</a>, <a href=\"https://spoonacular.com/recipes/vietnamese-fried-egg-banh-mi-recipe-banh-mi-trung-op-la-the-41453\">Vietnamese Fried Egg Banh Mi Recipe (banh Mi Trung Op La)- “the</a>, and <a href=\"https://spoonacular.com/recipes/vietnamese-dipping-sauce-594932\">Vietnamese Dipping Sauce</a> for similar recipes.",
+            "cuisines": [
+                "Vietnamese",
+                "Asian"
+            ],
+            "dishTypes": [
+                "side dish"
+            ],
+            "diets": [
+                "gluten free",
+                "dairy free",
+                "lacto ovo vegetarian"
+            ],
+            "occasions": [
+                "super bowl"
+            ],
+            "analyzedInstructions": [
+                {
+                    "name": "",
+                    "steps": [
+                        {
+                            "number": 1,
+                            "step": "Begin by preparing the pancake batter.",
+                            "ingredients": [
+                                {
+                                    "id": 0,
+                                    "name": "pancakes",
+                                    "localizedName": "pancakes",
+                                    "image": ""
+                                }
+                            ],
+                            "equipment": []
+                        },
+                        {
+                            "number": 2,
+                            "step": "Whisk together the rice flour, egg, sea salt, turmeric, cayenne and green chili. Gradually whisk in the coconut milk. The mixture should be a smooth, semi-thick batter that is of pouring consistency.",
+                            "ingredients": [
+                                {
+                                    "id": 12118,
+                                    "name": "coconut milk",
+                                    "localizedName": "coconut milk",
+                                    "image": "coconut-milk.png"
+                                },
+                                {
+                                    "id": 31015,
+                                    "name": "green chili pepper",
+                                    "localizedName": "green chili pepper",
+                                    "image": "chili-peppers-green.jpg"
+                                },
+                                {
+                                    "id": 20061,
+                                    "name": "rice flour",
+                                    "localizedName": "rice flour",
+                                    "image": "white-powder.jpg"
+                                },
+                                {
+                                    "id": 1012047,
+                                    "name": "sea salt",
+                                    "localizedName": "sea salt",
+                                    "image": "salt.jpg"
+                                },
+                                {
+                                    "id": 2043,
+                                    "name": "turmeric",
+                                    "localizedName": "turmeric",
+                                    "image": "turmeric.jpg"
+                                },
+                                {
+                                    "id": 2031,
+                                    "name": "ground cayenne pepper",
+                                    "localizedName": "ground cayenne pepper",
+                                    "image": "chili-powder.jpg"
+                                },
+                                {
+                                    "id": 1123,
+                                    "name": "egg",
+                                    "localizedName": "egg",
+                                    "image": "egg.png"
+                                }
+                            ],
+                            "equipment": [
+                                {
+                                    "id": 404661,
+                                    "name": "whisk",
+                                    "localizedName": "whisk",
+                                    "image": "whisk.png"
+                                }
+                            ]
+                        },
+                        {
+                            "number": 3,
+                            "step": "Add a little water if necessary. Cover and set aside while you prepare the rest of the food.Now prepare the sauce.",
+                            "ingredients": [
+                                {
+                                    "id": 0,
+                                    "name": "sauce",
+                                    "localizedName": "sauce",
+                                    "image": ""
+                                },
+                                {
+                                    "id": 14412,
+                                    "name": "water",
+                                    "localizedName": "water",
+                                    "image": "water.png"
+                                }
+                            ],
+                            "equipment": []
+                        },
+                        {
+                            "number": 4,
+                            "step": "Whisk together all of the ingredients and set aside.For the filling, combine the carrot, radish, green onions, green chilies and snow peas.",
+                            "ingredients": [
+                                {
+                                    "id": 31015,
+                                    "name": "green chili pepper",
+                                    "localizedName": "green chili pepper",
+                                    "image": "chili-peppers-green.jpg"
+                                },
+                                {
+                                    "id": 11291,
+                                    "name": "green onions",
+                                    "localizedName": "green onions",
+                                    "image": "spring-onions.jpg"
+                                },
+                                {
+                                    "id": 11300,
+                                    "name": "snow peas",
+                                    "localizedName": "snow peas",
+                                    "image": "snow-peas.jpg"
+                                },
+                                {
+                                    "id": 11124,
+                                    "name": "carrot",
+                                    "localizedName": "carrot",
+                                    "image": "sliced-carrot.png"
+                                },
+                                {
+                                    "id": 11429,
+                                    "name": "radish",
+                                    "localizedName": "radish",
+                                    "image": "radishes.jpg"
+                                }
+                            ],
+                            "equipment": [
+                                {
+                                    "id": 404661,
+                                    "name": "whisk",
+                                    "localizedName": "whisk",
+                                    "image": "whisk.png"
+                                }
+                            ]
+                        },
+                        {
+                            "number": 5,
+                            "step": "Put the herbs in another bowl and have your sprouts ready along with the mushrooms.",
+                            "ingredients": [
+                                {
+                                    "id": 11260,
+                                    "name": "mushrooms",
+                                    "localizedName": "mushrooms",
+                                    "image": "mushrooms.png"
+                                },
+                                {
+                                    "id": 11001,
+                                    "name": "sprouts",
+                                    "localizedName": "sprouts",
+                                    "image": "alfalfa-sprouts.png"
+                                },
+                                {
+                                    "id": 1002044,
+                                    "name": "herbs",
+                                    "localizedName": "herbs",
+                                    "image": "mixed-fresh-herbs.jpg"
+                                }
+                            ],
+                            "equipment": [
+                                {
+                                    "id": 404783,
+                                    "name": "bowl",
+                                    "localizedName": "bowl",
+                                    "image": "bowl.jpg"
+                                }
+                            ]
+                        },
+                        {
+                            "number": 6,
+                            "step": "Heat a teaspoon of oil over medium heat in a non-stick skillet. When hot, ladle 1/2 cup of batter into the pan and spread with the bottom of the ladle until you have a roughly 6 to 8 inch pancake.  Fry for 6 to 8 minutes or until you have small holes forming and the bottom is crispy brown, then flip and fry for another few minutes on the other side.",
+                            "ingredients": [
+                                {
+                                    "id": 0,
+                                    "name": "pancakes",
+                                    "localizedName": "pancakes",
+                                    "image": ""
+                                },
+                                {
+                                    "id": 0,
+                                    "name": "spread",
+                                    "localizedName": "spread",
+                                    "image": ""
+                                },
+                                {
+                                    "id": 4582,
+                                    "name": "cooking oil",
+                                    "localizedName": "cooking oil",
+                                    "image": "vegetable-oil.jpg"
+                                }
+                            ],
+                            "equipment": [
+                                {
+                                    "id": 404645,
+                                    "name": "frying pan",
+                                    "localizedName": "frying pan",
+                                    "image": "pan.png"
+                                },
+                                {
+                                    "id": 404630,
+                                    "name": "ladle",
+                                    "localizedName": "ladle",
+                                    "image": "ladle.jpg"
+                                }
+                            ],
+                            "length": {
+                                "number": 6,
+                                "unit": "minutes"
+                            }
+                        },
+                        {
+                            "number": 7,
+                            "step": "Transfer to a plate and keep warm in a 150 oven while you finish the rest of the pancakes  you should finish with 4 to 6 pancakes.To serve, scatter a portion of the vegetables, herbs and mushrooms over one half of the pancake.",
+                            "ingredients": [
+                                {
+                                    "id": 11583,
+                                    "name": "vegetable",
+                                    "localizedName": "vegetable",
+                                    "image": "mixed-vegetables.png"
+                                },
+                                {
+                                    "id": 11260,
+                                    "name": "mushrooms",
+                                    "localizedName": "mushrooms",
+                                    "image": "mushrooms.png"
+                                },
+                                {
+                                    "id": 0,
+                                    "name": "pancakes",
+                                    "localizedName": "pancakes",
+                                    "image": ""
+                                },
+                                {
+                                    "id": 1002044,
+                                    "name": "herbs",
+                                    "localizedName": "herbs",
+                                    "image": "mixed-fresh-herbs.jpg"
+                                }
+                            ],
+                            "equipment": [
+                                {
+                                    "id": 404784,
+                                    "name": "oven",
+                                    "localizedName": "oven",
+                                    "image": "oven.jpg"
+                                }
+                            ]
+                        },
+                        {
+                            "number": 8,
+                            "step": "Sprinkle some sauce over top and fold the pancake.",
+                            "ingredients": [
+                                {
+                                    "id": 0,
+                                    "name": "pancakes",
+                                    "localizedName": "pancakes",
+                                    "image": ""
+                                },
+                                {
+                                    "id": 0,
+                                    "name": "sauce",
+                                    "localizedName": "sauce",
+                                    "image": ""
+                                }
+                            ],
+                            "equipment": []
+                        },
+                        {
+                            "number": 9,
+                            "step": "Drizzle with more sauce and there you go.",
+                            "ingredients": [
+                                {
+                                    "id": 0,
+                                    "name": "sauce",
+                                    "localizedName": "sauce",
+                                    "image": ""
+                                }
+                            ],
+                            "equipment": []
+                        }
+                    ]
+                }
+            ],
+            "spoonacularSourceUrl": "https://spoonacular.com/vietnamese-pancakes-with-vegetables-herbs-and-a-fragrant-dipping-sauce-bnh-xo-716217"
+        }
+    ],
+    "offset": 99,
+    "number": 1,
+    "totalResults": 5017
+}
